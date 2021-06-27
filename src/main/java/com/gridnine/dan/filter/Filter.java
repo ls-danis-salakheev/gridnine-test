afbf66b0ddc989aa858035839bd53a03e225e3c2
sklyaba <sklyaba@ya.ru>
2021-06-27 22:16:00 +0300

    delete not necessary classes

--- src/main/java/com/gridnine/dan/filter/Filter.java
+++
@@ -1,9 +0,0 @@
-package com.gridnine.dan.filter;
-
-import java.util.List;
-
-public abstract class Filter<T> {
-
-    public abstract List<T> filterByTime(Filterable filter);
-
-}
--- src/main/java/com/gridnine/dan/random/TemporalRandomizer.java
+++
@@ -1,8 +0,0 @@
-package com.gridnine.dan.random;
-
-import java.time.temporal.Temporal;
-
-public interface TemporalRandomizer<T extends Temporal> {
-
-    T[] create();
-}
